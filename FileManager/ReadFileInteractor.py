9cf71727bafeb3851904e441c02a0ef1cde59d63
Simon-Noble <simon.noble@mail.utoronto.ca>
2024-08-15 17:48:59 -0400

    Implemented file reading. I am not satisfied with the implementation, but I will rework it when I have a better idea.

--- FileManager/ReadFileInteractor.py
+++ FileManager/ReadFileInteractor.py
@@ -3,11 +3,28 @@ This class manages the reading of information for later data analysis or other u
 
 
 """
+import pandas as pd
 
 
 class ReadFileInteractor:
     directory :str
+    index: int
+
     def __init__(self, directory: str):
         self.directory = directory
+        self.index = 0
 
-    def read(self):
+    def read_next(self):
+        """
+        read the next file in the directory and return it
+        """
+
+        with open(self.directory+'directory', 'r') as f:
+            file_name = f.readline()
+            for _ in range(self.index):
+                file_name = f.readline()
+
+        with open(self.directory+file_name, 'r') as f:
+            values = pd.read_json(f)
+
+        return values
